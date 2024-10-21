62e9dab6253ebef4bc410b14cb740fffd0ad01a9
ahmadsuherman <suhermana274@gmail.com>
2024-10-21 15:09:30 +0700

    [Pertemuan 5] - Komponen GUI Lanjutan

---
+++ src/PERTEMUAN5/ComboBoxExample.java
@@ -0,0 +1,36 @@
+package PERTEMUAN5;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+
+public class ComboBoxExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("JComboBox Example");
+        String[] options = {
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4"
+        };
+
+        final JComboBox < String > comboBox = new JComboBox < > (options);
+
+        comboBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	String selected = (String) comboBox.getSelectedItem();
+                System.out.println("Selected: " + selected);
+            }
+        });
+
+        frame.setLayout(null);
+        comboBox.setBounds(50, 50, 150, 20);
+        frame.add(comboBox);
+
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
